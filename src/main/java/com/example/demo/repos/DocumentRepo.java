b4b04cd14b764bc390f8c854852f95646eb17cf8
79033 <andreylenkom32@gmail.com>
2025-03-24 23:50:06 +0300

    Merge remote-tracking branch 'origin/master'

--- src/main/java/com/example/demo/repos/DocumentRepo.java
+++ src/main/java/com/example/demo/repos/DocumentRepo.java
@@ -6,9 +6,16 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface DocumentRepo extends JpaRepository<Document,Long> {
     @Query(value = "SELECT * FROM usr WHERE id = :id", nativeQuery = true)
     Optional<User> findByIdNative(@Param("id") Long id);
+    // В интерфейсе DocumentRepository
+
+    @Query(value = "SELECT DISTINCT d.* FROM documents d " +
+            "LEFT JOIN user_documents ud ON d.document_id = ud.document_id " +
+            "WHERE d.owner_user_id = :userId OR ud.user_id = :userId", nativeQuery = true)
+    List<Document> findAllDocumentsByUserId(@Param("userId") Long userId);
 }
---
+++ src/main/java/com/example/demo/rest/dto/DocumentDtos/DocumentListDTO.java
@@ -0,0 +1,15 @@
+package com.example.demo.rest.dto.DocumentDtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class DocumentListDTO {
+
+    private String title;
+    private Long id;
+
+}
