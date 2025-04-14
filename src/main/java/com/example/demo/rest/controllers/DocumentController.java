VorontsovAA812/new-repo

Merge remote-tracking branch 'origin/master'

src/main/java/com/example/demo/rest/controllers/DocumentController.java
@@ -1,12 +1,10 @@
 package com.example.demo.rest.controllers;
 
 import com.example.demo.config.SecurityUtils;
-import com.example.demo.domain.Document;
+import com.example.demo.rest.dto.DocumentDtos.ContentRequestDto;
 import com.example.demo.rest.dto.DocumentDtos.DocumentListDTO;
+import com.example.demo.rest.dto.DocumentDtos.DocumentResponse;
 import com.example.demo.rest.dto.DocumentDtos.NewDocumentRequest;
-import com.example.demo.rest.dto.UserDtos.NewUserRequest;
-import com.example.demo.rest.dto.UserDtos.UserDto;
-import com.example.demo.security.CustomUserDetails;
 import com.example.demo.service.DocumentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -15,12 +13,11 @@ import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 @Controller
-@RequestMapping("/api/v1/document")
+@RequestMapping("/api/v1/documents")
 public class DocumentController {
     private final DocumentService documentService;
     private  final  SecurityUtils securityUtils;
@@ -28,6 +25,17 @@ public class DocumentController {
     public DocumentController(DocumentService documentService, SecurityUtils securityUtils) {
         this.documentService = documentService;
         this.securityUtils = securityUtils;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ContentRequestDto> findById(@PathVariable Long id) {
+        return  ResponseEntity.ok(documentService.findById(id));
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<DocumentResponse> updateDocument(@PathVariable Long id, @RequestBody ContentRequestDto documentContentDto) {
+
+        return  ResponseEntity.ok(documentService.updateDocument(id,documentContentDto));
     }
 
 
@@ -40,6 +48,7 @@ public class DocumentController {
         // Возвращаем JSON с ключом "documentId"
         return ResponseEntity.ok(Map.of("documentId", documentId));
     }
+
 
     @GetMapping("/documentList")
     public ResponseEntity<List<DocumentListDTO>> getDocumentsForCurrentUser(Authentication authentication) {

src/main/java/com/example/demo/service/DocumentService.java
@@ -1,16 +1,17 @@
 package com.example.demo.service;
 
-import com.example.demo.domain.Document;
-import com.example.demo.domain.User;
+import com.example.demo.rest.dto.DocumentDtos.ContentRequestDto;
 import com.example.demo.rest.dto.DocumentDtos.DocumentListDTO;
+import com.example.demo.rest.dto.DocumentDtos.DocumentResponse;
 import com.example.demo.rest.dto.DocumentDtos.NewDocumentRequest;
 import org.springframework.security.core.Authentication;
 
-import javax.print.Doc;
 import java.util.List;
 
 public interface DocumentService {
     Long createDocument(NewDocumentRequest request, Authentication authentication);
     void addNewUserToDocument(Long userId, Long documentId);
     List<DocumentListDTO> getDocumentsForCurrentUser(Authentication authentication);
+    ContentRequestDto findById(Long id);
+    DocumentResponse updateDocument(Long id, ContentRequestDto contentRequestDto);
 }

src/main/java/com/example/demo/service/impl/DocumentServiceImpl.java
@@ -5,18 +5,20 @@ import com.example.demo.domain.*;
 
 import com.example.demo.repos.DocumentRepo;
 import com.example.demo.repos.UserDocumentRepo;
+import com.example.demo.rest.dto.DocumentDtos.ContentRequestDto;
 import com.example.demo.rest.dto.DocumentDtos.DocumentListDTO;
+import com.example.demo.rest.dto.DocumentDtos.DocumentResponse;
 import com.example.demo.rest.dto.DocumentDtos.NewDocumentRequest;
 import com.example.demo.security.CustomUserDetails;
 import com.example.demo.service.DocumentService;
 import com.example.demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
+import javax.print.Doc;
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
@@ -40,6 +42,11 @@ public class DocumentServiceImpl implements DocumentService {
 
     }
 
+    public DocumentResponse convertDocumentToResponse(Document document )
+    {
+        return  new DocumentResponse(document.getId(), document.getTitle(), document.getContent(), document.getOwnerUser().getUsername(),document.getCreatedAt(),document.getUpdatedAt());
+    }
+
 
     @Override
     public void addNewUserToDocument(Long userId, Long documentId) {
@@ -55,6 +62,19 @@ public class DocumentServiceImpl implements DocumentService {
         // Сохранить обновленного пользователя
         userService.save(user);
     }
+    @Override
+    public ContentRequestDto findById(Long id) {
+
+        Optional<Document> element = documentRepo.findById(id);
+        if (element.isPresent()) {
+            Document document =  element.get();
+            return new ContentRequestDto(document.getContent());
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Документ не найден");
+        }
+    }
+
+
     @Override
     public List<DocumentListDTO> getDocumentsForCurrentUser(Authentication authentication) {
 
@@ -142,6 +162,22 @@ public class DocumentServiceImpl implements DocumentService {
 
 
     }
+
+    @Override
+    public DocumentResponse updateDocument(Long id, ContentRequestDto documentContentDto)
+    {
+        Optional<Document> element = documentRepo.findById(id);
+        Document document = element.get();
+
+        document.setContent(documentContentDto.getContent());
+        document.setUpdatedAt(Instant.now());
+
+        documentRepo.save(document);
+
+        return convertDocumentToResponse(document);
+
+    }
+
 
 
 

VorontsovAA812/latex-collab-editor

src/main/java/com/example/demo/rest/controllers/DocumentController.java
@@ -1,12 +1,10 @@
 package com.example.demo.rest.controllers;
 
 import com.example.demo.config.SecurityUtils;
-import com.example.demo.domain.Document;
+import com.example.demo.rest.dto.DocumentDtos.ContentRequestDto;
 import com.example.demo.rest.dto.DocumentDtos.DocumentListDTO;
+import com.example.demo.rest.dto.DocumentDtos.DocumentResponse;
 import com.example.demo.rest.dto.DocumentDtos.NewDocumentRequest;
-import com.example.demo.rest.dto.UserDtos.NewUserRequest;
-import com.example.demo.rest.dto.UserDtos.UserDto;
-import com.example.demo.security.CustomUserDetails;
 import com.example.demo.service.DocumentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -15,12 +13,11 @@ import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 @Controller
-@RequestMapping("/api/v1/document")
+@RequestMapping("/api/v1/documents")
 public class DocumentController {
     private final DocumentService documentService;
     private  final  SecurityUtils securityUtils;
@@ -30,8 +27,19 @@ public class DocumentController {
         this.securityUtils = securityUtils;
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<DocumentResponse> findById(@PathVariable Long id) {
+        return  ResponseEntity.ok(documentService.findById(id));
+    }
 
-    @PostMapping("/create")
+    @PatchMapping("/{id}")
+    public ResponseEntity<DocumentResponse> updateDocument(@PathVariable Long id, @RequestBody NewDocumentRequest updateDocumentRequest) {
+
+        return  ResponseEntity.ok(documentService.updateDocument(id,updateDocumentRequest));
+    }
+
+
+    @PostMapping("")
     public ResponseEntity<Map<String, Long>> createDocument(@RequestBody NewDocumentRequest request, Authentication authentication) {
 
         // Передаем ID пользователя и данные документа в сервис
@@ -40,6 +48,7 @@ public class DocumentController {
         // Возвращаем JSON с ключом "documentId"
         return ResponseEntity.ok(Map.of("documentId", documentId));
     }
+
 
     @GetMapping("/documentList")
     public ResponseEntity<List<DocumentListDTO>> getDocumentsForCurrentUser(Authentication authentication) {

src/main/java/com/example/demo/service/DocumentService.java
@@ -1,16 +1,17 @@
 package com.example.demo.service;
 
-import com.example.demo.domain.Document;
-import com.example.demo.domain.User;
+import com.example.demo.rest.dto.DocumentDtos.ContentRequestDto;
 import com.example.demo.rest.dto.DocumentDtos.DocumentListDTO;
+import com.example.demo.rest.dto.DocumentDtos.DocumentResponse;
 import com.example.demo.rest.dto.DocumentDtos.NewDocumentRequest;
 import org.springframework.security.core.Authentication;
 
-import javax.print.Doc;
 import java.util.List;
 
 public interface DocumentService {
     Long createDocument(NewDocumentRequest request, Authentication authentication);
     void addNewUserToDocument(Long userId, Long documentId);
     List<DocumentListDTO> getDocumentsForCurrentUser(Authentication authentication);
+    DocumentResponse findById(Long id);
+    DocumentResponse updateDocument(Long id, NewDocumentRequest updateDocumentRequest);
 }

src/main/java/com/example/demo/service/impl/DocumentServiceImpl.java
@@ -5,18 +5,20 @@ import com.example.demo.domain.*;
 
 import com.example.demo.repos.DocumentRepo;
 import com.example.demo.repos.UserDocumentRepo;
+import com.example.demo.rest.dto.DocumentDtos.ContentRequestDto;
 import com.example.demo.rest.dto.DocumentDtos.DocumentListDTO;
+import com.example.demo.rest.dto.DocumentDtos.DocumentResponse;
 import com.example.demo.rest.dto.DocumentDtos.NewDocumentRequest;
 import com.example.demo.security.CustomUserDetails;
 import com.example.demo.service.DocumentService;
 import com.example.demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
+import javax.print.Doc;
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
@@ -40,6 +42,18 @@ public class DocumentServiceImpl implements DocumentService {
 
     }
 
+    public DocumentResponse convertDocumentToResponse(Document document )
+    {
+        return DocumentResponse.builder()
+                .id(document.getId())
+                .title(document.getTitle())
+                .content(document.getContent())
+                .ownerUsername(document.getOwnerUser().getUsername())
+                .createdAt(document.getCreatedAt())
+                .updatedAt(document.getUpdatedAt())
+                .build();
+    }
+
 
     @Override
     public void addNewUserToDocument(Long userId, Long documentId) {
@@ -55,6 +69,26 @@ public class DocumentServiceImpl implements DocumentService {
         // Сохранить обновленного пользователя
         userService.save(user);
     }
+    @Override
+    public DocumentResponse findById(Long id) {
+
+        Optional<Document> element = documentRepo.findById(id);
+        if (element.isPresent()) {
+            Document document =  element.get();
+            return DocumentResponse.builder()
+                    .id(document.getId())
+                    .title(document.getTitle())
+                    .content(document.getContent())
+                    .ownerUsername(document.getOwnerUser().getUsername())
+                    .createdAt(document.getCreatedAt())
+                    .updatedAt(document.getUpdatedAt())
+                    .build();
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Документ не найден");
+        }
+    }
+
+
     @Override
     public List<DocumentListDTO> getDocumentsForCurrentUser(Authentication authentication) {
 
@@ -111,9 +145,12 @@ public class DocumentServiceImpl implements DocumentService {
         document.setTitle(request.getTitle());
         document.setOwnerUser(author); // Обязательно устанавливаем владельца!
         document.setContent(request.getContent());
-
+        // Явно устанавливаем timestamp
+        document.setCreatedAt(Instant.now());
+        document.setUpdatedAt(Instant.now());
         // Сохраняем документ, чтобы получить его ID
         Document savedDocument = documentRepo.save(document);
+
 
 
 
@@ -142,6 +179,23 @@ public class DocumentServiceImpl implements DocumentService {
 
 
     }
+
+    @Override
+    public DocumentResponse updateDocument(Long id, NewDocumentRequest updateDocumentRequest)
+    {
+        Optional<Document> element = documentRepo.findById(id);
+        Document document = element.get();
+
+        document.setTitle(updateDocumentRequest.getTitle());
+        document.setContent(updateDocumentRequest.getContent());
+        document.setUpdatedAt(Instant.now());
+
+        documentRepo.save(document);
+
+        return convertDocumentToResponse(document);
+
+    }
+
 
 
 